38fe55169240158a9c351f1ddd6f59ff0d0579c7
dane <danerismendoza096@gmail.com>
2022-06-30 15:17:01 +0800

    v2

--- src/getarraymodeusingmap/GetArrayModeUsingMap.java
+++ src/getarraymodeusingmap/GetArrayModeUsingMap.java
@@ -7,42 +7,44 @@ import java.util.Map;
 
 public class GetArrayModeUsingMap {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,};
-        int higestCount = 1,higestMode=0,key=0;
-        ArrayList<Integer> modes = new ArrayList<Integer>();
-        HashMap<Integer, Integer> modeWithCount = new HashMap<>();
-         for (int i = 0; i <= arr.length-1; i++) {
-            if (modeWithCount.containsKey(arr[i])) {            
-                int c = modeWithCount.get(arr[i]);
-                modeWithCount.replace(arr[i], c + 1);
-            }
+        int arr[] = {1,2,3,4};
+        int higestCount = 1;     
+        Map<Integer, Integer> modeWithCount = new HashMap<Integer, Integer>();
+        ArrayList<Integer> arrList = new ArrayList<>();
+        
+         for (int i = 0; i < arr.length; i++) {
+            if (modeWithCount.containsKey(arr[i]))                
+                modeWithCount.replace(arr[i], modeWithCount.get(arr[i]) + 1);
             else  
                 modeWithCount.put(arr[i], 1);
         }
-        for (Map.Entry<Integer, Integer> i : modeWithCount.entrySet()) {
-            if (i.getValue() > higestCount){
-                higestMode = i.getKey();
-                higestCount++;       
-                key = i.getKey();
-            }                  
+         
+        Integer higestMode =  null;
+        for (Map.Entry<Integer, Integer> currentKey : modeWithCount.entrySet()) {
+            if (currentKey.getValue() > higestCount){
+                higestMode = currentKey.getKey();
+                higestCount++;
+            }
         } 
-         modeWithCount.remove(key); 
-        for (Map.Entry<Integer, Integer> i : modeWithCount.entrySet()) {
-            if (i.getValue() == higestCount)
-              modes.add(i.getKey());
-        } 
-        if(!modes.isEmpty())
-        modes.add(higestMode);
-        System.out.println("array: "+Arrays.toString(arr));    
-        if(modes.isEmpty())
-        System.out.println("Mode: "+higestMode);
-        else if(higestCount == 1)
-        System.out.println("no modes");
+        
+        if(higestCount!=1)    
+        for(Map.Entry<Integer, Integer> currentKey : modeWithCount.entrySet()){
+             if (currentKey.getValue() == higestCount)
+                arrList.add(currentKey.getKey());
+        }
+      
+        System.out.println("array: "+Arrays.toString(arr));  
+        if(higestCount==1)
+            System.out.println("no modes");
+        else if(arrList.size() == 1)
+            System.out.println("mode: "+higestMode);
         else
-        System.out.println("modes: "+modes.toString()); 
+            System.out.println("modes: "+Arrays.toString(arrList.toArray()));
+        
     }
-     /*code reserved        
-      System.out.println("value: "+i.getKey() + " Count: "+ i.getValue());
-      System.out.println("map: "+modeWithCount.toString());
+     /*code reserved 
+        map.keySet().toArray()[index]
+        System.out.println("value: "+i.getKey() + " Count: "+ i.getValue());
+        System.out.println("map: "+modeWithCount.toString());
     */
 }
